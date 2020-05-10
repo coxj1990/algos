7610728d8fd4d160013ab7fd8ef5466aca7e27a5
Jon <joncoxaz@gmail.com>
2020-05-10 08:45:36 -0700

    add invert binary tree

---
+++ utils/tree_utils.py
@@ -0,0 +1,8 @@
+def is_same_tree(root1, root2):
+    if not root1 and not root2:
+        return True
+    if not root1 or not root2:
+        return False
+    return root1.data == root2.data and \
+        is_same_tree(root1.left, root2.left) and \
+        is_same_tree(root1.right, root2.right)
