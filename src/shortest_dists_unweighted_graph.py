fe6076aa27f50ee7797b1fdfb6f8f54374865e54
Jon <joncoxaz@gmail.com>
2020-05-15 08:20:35 -0700

    add shortest dists in unweighted graph

---
+++ src/shortest_dists_unweighted_graph.py
@@ -0,0 +1,14 @@
+def shortest(G, start):
+    n = len(G)
+    dists = [float('inf') for _ in range(n)]
+    dists[start] = 0
+    visited = set([start])
+    q = [start]
+    while q:
+        curr = q.pop(0)
+        for child in G[curr]:
+            if child not in visited:
+                visited.add(child)
+                q.append(child)
+                dists[child] = dists[curr] + 1
+    return dists
