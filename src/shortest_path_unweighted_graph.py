d467dc003cb2dae54f70b6ad7951cf700edbe255
Jon <joncoxaz@gmail.com>
2020-05-16 13:26:09 -0700

    add shortest path unweighted graph

---
+++ src/shortest_path_unweighted_graph.py
@@ -0,0 +1,13 @@
+def shortest_path(G, start, dest):
+    q = [[start]]
+    visited = set([start])
+    while q:
+        curr_path = q.pop(0)
+        curr = curr_path[-1]
+        if curr == dest:
+            return curr_path
+        for child in G[curr]:
+            if child not in visited:
+                visited.add(child)
+                q.append(curr_path + [child])
+    return -1
