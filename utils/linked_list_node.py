f7468fa2912a088cb4d970d028932d02ea4341ae
Jon <joncoxaz@gmail.com>
2020-05-18 15:07:49 -0700

    add linked list utils

---
+++ utils/linked_list_node.py
@@ -0,0 +1,5 @@
+class LinkedListNode:
+
+    def __init__(self, val, nxt=None):
+        self.val = val
+        self.next = nxt
---
+++ utils/print_linked_list.py
@@ -0,0 +1,4 @@
+def print_linked_list(head):
+    while head:
+        print(head.val),
+        head = head.next
