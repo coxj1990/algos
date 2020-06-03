dea858b64070d3c38642ced952f68b74becf3da5
coxj1990 <joncoxaz@gmail.com>
2020-06-03 07:52:09 -0700

    add queue push and pop

---
+++ src/containers/queue_push_pop.py
@@ -0,0 +1,10 @@
+class Queue:
+
+    def __init__(self):
+        self.q = []
+
+    def push(self, x):
+        self.q.append(x)
+
+    def pop(self):
+        return self.q.pop(0)
---
+++ tests/containers/test_queue_push_pop.py
@@ -0,0 +1,17 @@
+from src.containers.queue_push_pop import Queue
+
+def test_trivial():
+    q = Queue()
+    q.push(1)
+    assert q.pop() == 1
+
+def test_nontrivial():
+    q = Queue()
+    n = 3
+    nums = list(range(n))
+    for e in nums:
+        q.push(e)
+    popped = []
+    for _ in range(n):
+        popped.append(q.pop())
+    assert popped == nums
