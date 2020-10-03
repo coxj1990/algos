adec7b70f02a823df1423bb0e043764c008972f4
coxj1990 <joncoxaz@gmail.com>
2020-10-03 14:58:25 -0700

    Simplify dijkstra shortest path

--- src/graphs/shortest_path_nonnegative_weights.py
+++ src/graphs/shortest_path_nonnegative_weights.py
@@ -6,8 +6,6 @@ from collections import defaultdict
 import heapq as hq
 
 def shortest_path(G, start, dest):
-    dists = defaultdict(lambda: float('inf'))
-    dists[start] = 0
     q = [(0, [start])]
     visited = set([start])
     while q:
@@ -16,11 +14,7 @@ def shortest_path(G, start, dest):
         if curr == dest:
             return curr_path
         visited.add(curr)
-        for child, next_cost in G[curr]:
-            if child in visited:
-                continue
-            cost = curr_cost + next_cost
-            if cost < dists[child]:
-                dists[child] = cost
-                hq.heappush(q, (cost, curr_path + [child]))
+        for child, child_cost in G[curr]:
+            if child not in visited:
+                hq.heappush(q, (curr_cost + child_cost, curr_path + [child]))
     return -1
