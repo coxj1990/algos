49849e321653b74aa5136dbfc3f328f8afb92939
coxj1990 <joncoxaz@gmail.com>
2022-02-16 06:56:59 -0800

    improve max sum subarray tests and comments

--- src/arrays/max_sum_subarray.py
+++ src/arrays/max_sum_subarray.py
@@ -1,9 +1,9 @@
-def get_max_sum(L):
-    if not L:
-        return 0
-    max_sum = -float('inf')
-    curr_sum = -float('inf')
-    for e in L:
+# brute force solution is O(n^3)
+# dynamic programming solution is O(n)
+def get_max_sum(arr):
+    max_sum = 0
+    curr_sum = 0
+    for e in arr:
         curr_sum = max(e, curr_sum + e)
         max_sum = max(max_sum, curr_sum)
     return max_sum
--- tests/arrays/test_max_sum_subarray.py
+++ tests/arrays/test_max_sum_subarray.py
@@ -1,9 +1,9 @@
 from src.arrays.max_sum_subarray import get_max_sum
 
-def test_trivial():
-    L = []
-    assert get_max_sum(L) == 0
+def test_empty_array_has_zero_sum():
+    arr = []
+    assert get_max_sum(arr) == 0
 
-def test_nontrivial():
-    L = [-2, -3, 4, -1, -2, 1, 5, -3]
-    assert get_max_sum(L) == 7
+def test_computes_max_sum():
+    arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+    assert get_max_sum(arr) == 7
