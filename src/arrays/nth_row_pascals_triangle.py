0d4bc404548b97561b3c287a2578b54a320e7d94
Jon Cox <joncoxaz@gmail.com>
2023-08-02 19:18:44 -0700

    remove nth row pascals triangle

--- src/arrays/nth_row_pascals_triangle.py
+++
@@ -1,5 +0,0 @@
-def get_nth_row(n):
-    row = [1]
-    for _ in range(n):
-        row = [x + y for x, y in zip([0] + row, row + [0])]
-    return row
--- tests/arrays/test_nth_row_pascals_triangle.py
+++
@@ -1,7 +0,0 @@
-from src.arrays.nth_row_pascals_triangle import get_nth_row
-
-def test_trivial():
-    assert get_nth_row(0) == [1]
-
-def test_nontrivial():
-    assert get_nth_row(4) == [1, 4, 6, 4, 1]
