7e852594fb182a4ea70a4d4d577a969074cc370b
Jon Cox <joncoxaz@gmail.com>
2023-08-03 20:24:28 -0700

    better names for trie tests

--- tests/trees/test_make_trie.py
+++ tests/trees/test_make_trie.py
@@ -1,11 +1,11 @@
 from src.trees.make_trie import make_trie
 
-def test_trivial():
+def test_make_one_word_trie():
     words = ['test']
     trie = {'t': {'e': {'s': {'t': {'end': 'end'}}}}}
     assert make_trie(words) == trie
 
-def test_nontrivial():
+def test_make_multiple_word_trie():
     words = ['bar', 'baz']
     trie = {'b': {'a': {'z': {'end': 'end'}, 'r': {'end': 'end'}}}}
     assert make_trie(words) == trie
